604e7884ba586453a1a196f49bd898121b7a19c7
DaxStrife <hectorwar57@yahoo.com>
2019-06-29 20:55:32 -0500

    final

--- Rancho_San_Miguel_v2/Rancho_San_Miguel_v2/settings.py
+++ Rancho_San_Miguel_v2/Rancho_San_Miguel_v2/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xd$xh#taz2s0pi0gc3hdicz0z1-=xotknblabcqo((pk*fnznq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\daxx\desktop\fabrica\rancho_san_miguel_v2\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
